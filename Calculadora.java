4d4611817eb06e56a9eba605301d50436d564e6f
unknown <adri.go2004@gmail.com>
2023-05-20 17:24:47 +0200

    Cambio Adrian

--- Calculadora.java
+++ Calculadora.java
@@ -108,5 +108,14 @@ public class Calculadora {
 
         return Long.toOctalString(decimal);
     }
+
+	//Modificacion Adrian Gregorio
+	public static double calcularRaizCuadrada(double numero) {
+        if (numero < 0) {
+            throw new IllegalArgumentException("No se puede calcular la raíz cuadrada de un número negativo");
+        }
+        
+        return Math.sqrt(numero);
+    }
 	
 }
